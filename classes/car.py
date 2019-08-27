5b2781dc1e4860bf48a42f0e4e657b3106547a26
Gowtham Sridhar <42664953+Gowtham-Sridhar@users.noreply.github.com>
2019-08-27 17:08:49 +0530

    inheritance

--- classes/car.py
+++ classes/car.py
@@ -1,50 +1,73 @@
-
-class Car():
-    ''' simple attempt to represent a car. '''
+class Car:
 
     def __init__(self, make, model, year):
-        ''' initialize attributes to describe a car. '''
         self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
-    
-    def get_descriptive_name(self):
-        ''' return neatly formatted descriptive name. '''
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
 
-    def update_odometer(self, mileage):
-        ''' 
-        set the odometer reading to given value. 
-        reject the change if it attempt to change the odometer back.
-        '''
+    def fill_gas_tank(self):
+        print('tank filled.')
+
+    def get_descriptive_name(self):
+        longname = str(self.year) + ' ' + self.make + ' ' + self.model
+        print(longname.title())
+
+    def read_odometer(self):
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+
+    def update_odometer_reading(self, mileage):
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
-            print('you cannot rollback the odometer.')
+            print('access denied.')
 
-    def increment_odometer(self, mileage):
-        ''' add given amount to odometer reading. '''
+    def increment_odometer_reading(self, mileage):
         if mileage > 0:
             self.odometer_reading += mileage
         else:
-            print('you cannot rollback the odometer.')
+            print('access denied.')
     
-    def read_odometer(self):
-        ''' print a statement showing car mileage. '''
-        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+class Battery:
 
+    def __init__(self, battery_size = 75):
+        self.battery_size = battery_size
 
-my_car = Car('audi', 'a4', 2016)
-# my_car.odometer_reading = 30
-print(my_car.get_descriptive_name())
-my_car.update_odometer(40)
-my_car.read_odometer()
-my_car.update_odometer(10)
-my_car.read_odometer()
+    def battery_upgrade(self):
+        if self.battery_size != 85:
+            self.battery_size = 85
+        else:
+            print('battery already upgraded.')
+        
 
-print('\nincrement odometer by: 30')
-my_car.increment_odometer(30)
-# my_car.update_odometer(45)
-my_car.read_odometer()
+    def describe_battery(self):
+        print('This car has a ' + str(self.battery_size) + '-kWh battery.')
+
+    def get_range(self):
+        if self.battery_size == 75:
+            miles = 240
+        elif self.battery_size == 85:
+            miles = 270
+
+        message = 'This car go approx. ' + str(miles) + ' miles on full charge'
+        print(message)
+    
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    # @override
+    def fill_gas_tank(self):
+        pass
+
+car = ElectricCar('tesla', 's', 2019)
+car.battery.describe_battery()
+car.battery.get_range()
+
+car.battery.battery_upgrade()
+car.battery.describe_battery()
+car.battery.get_range()
+
+car.battery.battery_upgrade()
